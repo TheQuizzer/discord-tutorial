534f20d4e5191d93770ffa23ed0ebaf26a86352a
TheQuizzer <85128882+TheQuizzer@users.noreply.github.com>
2021-07-06 20:04:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,37 @@
-const { Client, Collection } = require("discord.js");
-const { config } = require("dotenv");
-const {prefix, token } = require("./config.json")
-const { CanvasSenpai } = require("canvas-senpai")
-const canva = new CanvasSenpai();
-const db = require("quick.db"); 
+const Aoijs = require("aoi.js")
 
-const discord = require("discord.js");
-const client = new discord.Client({
- 
-  disableEveryone: false
-});
+const bot = new Aoijs.Bot({
+fetchInvites: true,
+token: "top secret bot token", //Discord Bot Token
+prefix: "prefix", //Customizable
+fetchInvites: true
+})
+bot.onMessage() //Allows to run Commands
 
-require('discord-buttons')(client)
-// go to package to update some stuff
-client.commands = new Collection();
-client.aliases = new Collection();
-
-
-// Run the command loader
-["command"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-});
-
-client.on("ready", () => {
-    console.log(`Hi, ${client.user.username} is now online!`);
-
-    client.user.setPresence("I am Devil") 
+bot.command({
+name: "ping",
+code: `$ping Pong!` 
 })
 
-client.on("message", async message => {
+bot.status({
+  text: "coding with the quizzer",
+  type: "COMPETING",
+  time: 10
+})
 
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
+bot.command({
+name: "command name", //Trigger name (command name)
+code: `
+$title[embed title]
+$description[
 
-    // If message.member is uncached, cache it.
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd.length === 0) return;
-
-    // Get the command
-    let command = client.commands.get(cmd);
-    // If none is found, try to find it by alias
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-    // If a command is finally found, run the command
-    if (command) 
-        command.run(client, message, args);
-});
-
-client.login('YOUR TOP SECRET BOT TOKEN HERE');
+I like coding discord bots yay lets gooooooo
 
 
+]
+
+$footer[footer]
+$color[RANDOM]
+
+`
+}); //Code
